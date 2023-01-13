a5f0108f580d45fc6078af9d7314127f4b3a1ac2
sircollo <sircollo71@gmail.com>
2023-01-13 11:19:30 +0300

    Fix: page going blank on refresh, add barchart feature

--- src/pages/CovidInfo.jsx
+++ src/pages/CovidInfo.jsx
@@ -1,23 +1,39 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, PureComponent } from "react";
 import Logo from "../assets/virus.png";
 import { FaSearchLocation } from "react-icons/fa";
 import { countries } from "country-data";
-import { GiDeathSkull } from "react-icons/gi"
-import { CiTempHigh } from "react-icons/ci"
-import { BiNotepad } from "react-icons/bi"
-import { GiRadioactive } from "react-icons/gi"
-import { GrStatusCriticalSmall } from "react-icons/gr"
-import { FcDataRecovery } from "react-icons/fc"
-import { FaViruses } from "react-icons/fa"
+import { GiDeathSkull } from "react-icons/gi";
+import { CiTempHigh } from "react-icons/ci";
+import { BiNotepad } from "react-icons/bi";
+import { GiRadioactive } from "react-icons/gi";
+import { GrStatusCriticalSmall } from "react-icons/gr";
+import { FcDataRecovery } from "react-icons/fc";
+import { FaViruses } from "react-icons/fa";
+import { FaTemperatureHigh} from "react-icons/fa"
 import axios from "axios";
-export default function CovidInfo() {
-  const [country, setCountry] = useState(null);
-  const [info, setInfo] = useState(null);
+import {
+  BarChart,
+  Bar,
+  Cell,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+  ResponsiveContainer,
+} from "recharts";
 
+export default function CovidInfo() {
+  const [country, setCountry] = useState();
+  const [info, setInfo] = useState();
+  const [chartData, setChartData] = useState([])
+  const {cases, deaths, population, tests} = chartData;
+  
   const options = {
     method: "GET",
     url: `https:ipinfo.io/json?token=a3477223ca0a15`,
   };
+  
   const apiOptions = {
     method: "GET",
     url: "https://covid-193.p.rapidapi.com/statistics",
@@ -32,25 +48,56 @@ export default function CovidInfo() {
     axios
       .request(apiOptions)
       .then(function (response) {
-        console.log(response.data.response[0]);
+        // console.log(response);
         setInfo(response.data.response);
+        const data = response.data.response[0]
+        setChartData(response.data.response[0])
+        console.log(response.data.response[0]);
+        return data
+        
       })
       .catch(function (error) {
         console.error(error);
       });
   }
-  // useEffect(() => {
-  //     if(navigator.geolocation){
-  //         axios.request(options).then(function (response){
-  //             const country_code = (response.data.country)
-  //             setCountry(countries[`${country_code}`].name)
-  //         }).catch(function (error){
-  //             console.log(error)
-  //             setCountry("Kenya")
-  //         });
-  //     }
-  //   });
+  async function getInitialCountry(e){
+    e.preventDefault(e)
+    if(navigator.geolocation){
+      axios.request(options).then(async function(response){   
+        const country_code = response.data.country
+        // setCountry(countries[`${country_code}`].name)
+      }).catch(function (error) {
+        console.error(error);
+      })
+    }
+   
+  }
+  
+  useEffect(() => {
+    (async () => {
+      let res = await getInitialCountry();
+      if (res) {
+      }
+    })();
+  });
+  const data = [
+      {
+        // name: "Cases",
+        // new_cases: cases.new,
+        // recovered: cases.recovered,
+        // active: cases.active,
+        // total_cases: cases.total,
+        // new_deaths: deaths.new,
+        // total_deaths: deaths.total,
+        // tests: deaths.total,
+  
+      },
+  
+      
+    ];
+   
 
+  
   return (
     <div className="bg-custom h-screen">
       <header className="sticky top-0 z-50 flex flex-wrap justify-between items-center py-2 px-3 max-w-6xl mx-auto">
@@ -105,35 +152,59 @@ export default function CovidInfo() {
                 </thead>
                 <tbody>
                   <tr>
-                    <td className="flex items-center space-x-3 py-2"><BiNotepad className="text-2xl mr-2 text-red-400" />New Cases</td>
+                    <td className="flex items-center space-x-3 py-2">
+                      <BiNotepad className="text-2xl mr-2 text-red-400" />
+                      New Cases
+                    </td>
                     <td>{info[0].cases.new}</td>
                   </tr>
                   <tr>
-                    <td className="flex items-center space-x-3 py-2"><GiRadioactive className="text-2xl mr-2 text-red-400" />Active Cases</td>
+                    <td className="flex items-center space-x-3 py-2">
+                      <GiRadioactive className="text-2xl mr-2 text-red-400" />
+                      Active Cases
+                    </td>
                     <td>{info[0].cases.active}</td>
                   </tr>
                   <tr>
-                    <td className="flex items-center space-x-3 py-2"><GrStatusCriticalSmall className="text-2xl mr-2 text-red-400" />Critical</td>
+                    <td className="flex items-center space-x-3 py-2">
+                      <GrStatusCriticalSmall className="text-2xl mr-2 text-red-400" />
+                      Critical
+                    </td>
                     <td>{info[0].cases.critical}</td>
                   </tr>
                   <tr>
-                    <td className="flex items-center space-x-3 py-2"><FcDataRecovery className="text-2xl mr-2 text-red-400" />Recovered</td>
+                    <td className="flex items-center space-x-3 py-2">
+                      <FcDataRecovery className="text-2xl mr-2 text-red-400" />
+                      Recovered
+                    </td>
                     <td>{info[0].cases.recovered}</td>
                   </tr>
                   <tr>
-                    <td className="flex items-center space-x-3 py-2"><GiDeathSkull className="text-2xl mr-2 text-red-400" />Deaths (new)</td>
+                    <td className="flex items-center space-x-3 py-2">
+                      <GiDeathSkull className="text-2xl mr-2 text-red-400" />
+                      Deaths (new)
+                    </td>
                     <td>{info[0].deaths.new}</td>
                   </tr>
                   <tr>
-                    <td  className="flex items-center space-x-3 py-2"><FaViruses className="text-2xl mr-2 text-red-400" />Total Cases</td>
+                    <td className="flex items-center space-x-3 py-2">
+                      <FaViruses className="text-2xl mr-2 text-red-400" />
+                      Total Cases
+                    </td>
                     <td>{info[0].cases.total}</td>
                   </tr>
-                  <tr >
-                    <td className="flex items-center space-x-3 py-2"><GiDeathSkull className="text-2xl mr-2 text-red-400" />Total deaths</td>
+                  <tr>
+                    <td className="flex items-center space-x-3 py-2">
+                      <GiDeathSkull className="text-2xl mr-2 text-red-400" />
+                      Total deaths
+                    </td>
                     <td>{info[0].deaths.total}</td>
                   </tr>
                   <tr>
-                    <td className="flex items-center space-x-3 py-2"><CiTempHigh className="text-2xl mr-2 text-red-400"/>Total tests</td>
+                    <td className="flex items-center space-x-3 py-2">
+                      <FaTemperatureHigh className="text-2xl mr-2 text-red-400" />
+                      Total tests
+                    </td>
                     <td>{info[0].tests.total}</td>
                   </tr>
                 </tbody>
@@ -141,15 +212,41 @@ export default function CovidInfo() {
             </div>
           </div>
           <div className="">
-            <p className="text-3xl text-white">Country Information</p>
+            <p className="text-3xl text-white">Graph Information</p>
+            {chartData && (
+              <BarChart
+              // width=
+              // height={300}
+              data={data}
+              margin={{
+                top: 5,
+                right: 30,
+                left: 20,
+                bottom: 5,
+              }}
+            className="w-full">
+              <CartesianGrid strokeDasharray="3 3" />
+              <XAxis dataKey="name" />
+              <YAxis />
+              <Tooltip />
+              <Legend />
+              <Bar dataKey="new_cases" fill="#8884d8" />
+              <Bar dataKey="recovered" fill="#82ca9d" />
+              <Bar dataKey="active" fill="#82ca9d" />
+              <Bar dataKey="new_deaths" fill="#82ca9d" />
+              <Bar dataKey="total_deaths" fill="#82ca9d" />
+              <Bar dataKey="tests" fill="#82ca9d" />
+            </BarChart>
+            )}
+            
           </div>
         </div>
       )}
-      {info &&(
+      {info && (
         <div className="max-w-6xl mx-auto mt-12 flex flex-col">
-        <p className="italic text-white text-sm">Date: {info[0].time}</p>
-      </div>
-      )}     
+          <p className="italic text-white text-sm">Date: {info[0].time}</p>
+        </div>
+      )}
     </div>
   );
 }
